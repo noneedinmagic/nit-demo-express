fe9bad8b45fcb595af98c2f0c5e8898798c8030b
Oleg <noneedinmagic@gmail.com>
2018-11-11 22:13:13 +0200

    Add some more Hello World examples to /hello view

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8841;
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.send('Hello NIT!'));
+
+app.get('/hello', (req, res) => res.render('hello', {testVar: 'testValue'}));
+app.get('/hello/:name', (req, res) => res.render('hello', {name: req.params.name}));
 
 app.listen(port, () => console.log(`NIT App is running on port ${port}...`));
